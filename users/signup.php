efbb86aba85fdeef1ccd6c27c6c021c759124bfc
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-18 21:49:48 +0800

    Update signup.php

--- users/signup.php
+++ users/signup.php
@@ -395,11 +395,12 @@ input[type="text"]:hover, input[type="password"]:hover {
                     <option value="staff">Staff</option>
                 </select>
             </div>
--->
+
             <div class="form-group field space">
                 <span class="fa bi bi-geo-fill" style="font-size: 20px"></span>
                 <input type="text" name="place" id="place" placeholder="Place" readonly class="form-control">
             </div>
+-->
             <div class="form-group field space">
                 <span class="fa bi bi-person-video2" style="font-size: 20px"></span>
                 <input type="text" name="LicenseNumber" maxlength="10" pattern="[0-9]*" placeholder="License Number" required="true" class="form-control">
