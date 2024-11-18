c328daa503dad08060767078e01370effae0f132
reignya <rhyniaimpas7@gmail.com>
2024-11-18 23:22:22 +0800

    Update dashboard.php

--- users/dashboard.php
+++ users/dashboard.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 error_reporting(0);
-include('../DBconnection/dbconnection.php');
+include('includes/dbconnection.php');
 error_reporting(0);
 if (strlen($_SESSION['vpmsuid']==0)) {
   header('location:logout.php');
@@ -317,7 +317,8 @@ setTimeout(function() {
 
 
 </script>
-
+<script>
+    </script>
 </body>
 </html>
 <?php } ?>
