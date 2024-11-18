237004485b2ce9d0dca6c50cb0bab5076c66c1ff
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-18 21:43:01 +0800

    Update signup.php

--- users/signup.php
+++ users/signup.php
@@ -382,7 +382,7 @@ input[type="text"]:hover, input[type="password"]:hover {
             </button>
         </div>
 
-        <!-- Page 2 -->
+        <!-- Page 2 
         <div id="page2" style="display: none;">
             <div class="form-group field space">
                 <span class="fa bi bi-person-lines-fill" style="font-size: 20px"></span>
@@ -398,6 +398,7 @@ input[type="text"]:hover, input[type="password"]:hover {
                 <span class="fa bi bi-geo-fill" style="font-size: 20px"></span>
                 <input type="text" name="place" id="place" placeholder="Place" readonly class="form-control">
             </div>
+-->
             <div class="form-group field space">
                 <span class="fa bi bi-person-video2" style="font-size: 20px"></span>
                 <input type="text" name="LicenseNumber" maxlength="10" pattern="[0-9]*" placeholder="License Number" required="true" class="form-control">
