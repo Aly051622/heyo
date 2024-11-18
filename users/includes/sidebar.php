8afa46c8bc90e77ed44e02b7d5bc9a1c3b12f520
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-18 22:29:25 +0800

    Update sidebar.php

--- users/includes/sidebar.php
+++ users/includes/sidebar.php
@@ -1,3 +1,8 @@
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <script src="https://kit.fontawesome.com/your-kit-code.js" crossorigin="anonymous"></script>
+
+</head>
 <aside id="left-panel" class="left-panel">
     <nav class="navbar navbar-expand-sm navbar-default">
         <div id="main-menu" class="main-menu collapse navbar-collapse">
@@ -9,22 +14,26 @@
                 </li>
                 <li>
                     <a href="view-vehicle.php">
-                        <i class="menu-icon ti-truck"></i>View Vehicle
+                        <i class="menu-icon fa fa-truck"></i>View Vehicle
                     </a>
                 </li>
-                <!--
                 <li>
-                    <a href="maps.php">
-                        <i class="menu-icon ti-map"></i>Maps
+                    <a href="view-vehicle.php">
+                    <i class="menu-icon fa fa-car-side"></i> Owned Vehicle/s
                     </a>
                 </li>
--->
+                <li>
+                    <a href="vehicle-transac.php">
+                        <i class="menu-icon fa fa-address-book"></i>Vehicle Logs
+                    </a>
+                </li>
+               
                 <li>
                     <a href="service.php">
-                        <i class="bi bi-headset"></i>ChatConcern
+                        <i class="menu-icon fa fa-headset"></i>Chat Concern
                     </a>
                 </li>
             </ul>
-        </div><!-- /.navbar-collapse -->
+        </div>
     </nav>
 </aside>
