2cbfdebf93c2be89b503c1807989e9129fcf1f55
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-18 21:30:48 +0800

    Update sidebar.php

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,13 +1,10 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
 <style>
     .left-panelbg {
-        box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, 
-            rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, 
-            rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
-            padding-bottom: 5px;
             font-size:12px;
     }
-    
 </style>
 
 <div class="left-panelbg">
@@ -24,8 +21,8 @@
                             <i class="menu-icon fa fa-list-alt"></i>Vehicle Category
                         </a>
                         <ul class="sub-menu children dropdown-menu">
-                            <li><i class="fa fa-road"></i><a href="add-category.php">Add Vehicle Category</a></li>
-                            <li><i class="fa fa-road"></i><a href="manage-category.php">Manage Vehicle Category</a></li>
+                            <li><i class="menu-icon fa fa-road"></i><a href="add-category.php">Add Vehicle Category</a></li>
+                            <li><i class="menu-icon fa bi bi-p-square-fill"></i><a href="manage-category.php">Manage Vehicle Category</a></li>
                         </ul>
                     </li>
 
@@ -38,18 +35,19 @@
                             <i class="menu-icon fa fa-th"></i>Manage Vehicle
                         </a>
                         <ul class="sub-menu children dropdown-menu">
-                            <li><i class="menu-icon fa fa-automobile"></i><a href="manage-incomingvehicle.php">Manage In Vehicle</a></li>
-                            <li><i class="menu-icon fa fa-automobile"></i><a href="manage-outgoingvehicle.php">Manage Out Vehicle</a></li>
+                             <li><i class="menu-icon fa fa-user-circle-o"> <a href="manage-reg.php"></i>Manage Registered Client Vehicles </a></li>
+                            <li><i class="menu-icon bi bi-car-front-fill"></i><a href="manage-incomingvehicle.php">Manage In Vehicle</a></li>
+                            <li><i class="menu-icon bi bi-car-front"></i><a href="manage-outgoingvehicle.php">Manage Out Vehicle</a></li>
                         </ul>
                     </li>
 
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <i class="menu-icon fa bi bi-qr-code"></i>QR Code Scanner
+                            <i class="menu-icon fa fa-solid fa-qrcode"></i>QR Code Scanner
                         </a>
                         <ul class="sub-menu children dropdown-menu">
-                            <li><i class="menu-icon fa-qrcode"></i><a href="qrlogin.php">Login Scanner</a></li>
-                            <li><i class="menu-icon fa-qrcode"></i><a href="qrlogout.php">Logout Scanner</a></li>
+                            <li><i class="menu-icon fa bi bi-qr-code-scan"></i><a href="qrlogin.php">Login Scanner</a></li>
+                            <li><i class="menu-icon fa bi bi-qr-code"></i><a href="qrlogout.php">Logout Scanner</a></li>
                         </ul>
                     </li>
 
@@ -66,10 +64,6 @@
                         </ul>
                     </li>
 
-                    <li>
-                        <a href="reg-users.php"><i class="menu-icon fa fa-address-book"></i>Client Information</a>
-                    </li>
-
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="menu-icon fa bi bi-card-checklist"></i>Validation
@@ -78,29 +72,25 @@
                             <li><i class="menu-icon fa bi bi-journal-text"></i><a href="validation.php">Validate</a></li>
                             <li><i class="menu-icon fa bi bi-journal-check"></i><a href="validated.php">Validated</a></li>
                             <li><i class="menu-icon fa bi bi-journal-x"></i><a href="unvalidated.php">Unvalidated</a></li>
-                            <li><i class="menu-icon bi bi-journal-minus"></i><a href="invalidated.php">InValidated</a>
-
+                            <li><i class="menu-icon fa bi bi-journal-minus"></i><a href="invalidated.php">InValidated</a>
                         </ul>
                     </li>
 
-                    <li>
-                        <a href="register.php"><i class="menu-icon fa fa-user-circle-o"></i>Register Client</a>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="menu-icon fa fa-address-book"></i>Client Management</a>
+                        
+                            <ul class="sub-menu children dropdown-menu">
+                                <li><i class="menu-icon fa fa-user-circle-o"></i><a href="register.php">Register Client</a></li>
+                                <li><i class="menu-icon fa fa-address-book"></i><a href="reg-users.php">User Information</a></li>
+                                <li><i class="menu-icon  fa bi bi-chat-dots-fill"></i><a href="admin_comments.php">Comment</a></li>
+                                <li><i class="menu-icon fa  bi bi-envelope-paper-heart"></i><a href="admin_feedbacks.php">Feedback</a></li>
+                                <li><i class="menu-icon fa  bi bi-headset"></i><a href="admin_service.php">Customer Service</a></li>
+                            </ul>
                     </li>
 
                     <li>
                         <a href="manage-slot.php"><i class="menu-icon fa  bi bi-geo-fill"></i> Add Area and Slot</a>
-                    </li>
-
-                    <li>
-                        <a href="admin_comments.php"><i class="menu-icon  fa bi bi-chat-dots-fill"></i> Comment</a>
-                    </li>
-
-                    <li>
-                        <a href="admin_feedbacks.php"><i class="menu-icon fa  bi bi-envelope-paper-heart"></i> Feedback</a>
-                    </li>
-
-                    <li>
-                        <a href="admin_service.php"><i class="menu-icon fa  bi bi-headset"></i> Customer Service</a>
                     </li>
                 </ul>
             </div><!-- /.navbar-collapse -->
