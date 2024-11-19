5b360a2506722ba0ed1b0904edc79c2683d48050
reignya <rhyniaimpas7@gmail.com>
2024-11-19 17:30:09 +0800

    Update add-vehicle.php

--- admin/add-vehicle.php
+++ admin/add-vehicle.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-date_default_timezone_set('Asia/Manila');
 error_reporting(E_ALL); // Enable error reporting for debugging
 ini_set('display_errors', 1);
+date_default_timezone_set('Asia/Manila');
 
 include('includes/dbconnection.php');
 
