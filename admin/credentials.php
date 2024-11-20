dcc040a463d48ecfacda5f1e60f759572cba096a
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-20 15:23:41 +0800

    Update credentials.php

--- admin/credentials.php
+++ admin/credentials.php
@@ -4,54 +4,44 @@ include 'includes/dbconnection.php';
 
 // Fetch all users' details
 $queryAllUsers = "
-    SELECT , 
+    SELECT 
            FirstName, 
            LastName, 
            Email, 
            MobileNumber, 
-          
            or_image, 
            cr_image, 
            nv_image, 
            profile_pictures
-          
-           
     FROM tblregusers
 ";
 
 $resultAllUsers = mysqli_query($con, $queryAllUsers);
 
-if (mysqli_num_rows($resultAllUsers) > 0) {
+if ($resultAllUsers && mysqli_num_rows($resultAllUsers) > 0) {
     echo "<h1>All Users</h1>";
     echo "<table border='1'>";
     echo "<tr>
-           
             <th>First Name</th>
             <th>Last Name</th>
             <th>Email</th>
             <th>Mobile Number</th>
-           
             <th>OR Image</th>
             <th>CR Image</th>
             <th>NV Image</th>
             <th>Profile Picture</th>
-            
-            
           </tr>";
 
     while ($row = mysqli_fetch_assoc($resultAllUsers)) {
         echo "<tr>
-               
                 <td>{$row['FirstName']}</td>
                 <td>{$row['LastName']}</td>
                 <td>{$row['Email']}</td>
                 <td>{$row['MobileNumber']}</td>
-              
                 <td><img src='../uploads/{$row['or_image']}' alt='OR Image' width='100'></td>
                 <td><img src='../uploads/{$row['cr_image']}' alt='CR Image' width='100'></td>
                 <td><img src='../uploads/{$row['nv_image']}' alt='NV Image' width='100'></td>
                 <td><img src='../uploads/{$row['profile_pictures']}' alt='Profile Picture' width='100'></td>
-             
               </tr>";
     }
 
