e903f5686df20963bcf60a171a66e0377bced041
reignya <rhyniaimpas7@gmail.com>
2024-11-24 13:49:44 +0800

    Update service.php

--- users/service.php
+++ users/service.php
@@ -28,6 +28,7 @@ if (strlen($_SESSION['vpmsuid']==0)) {
 <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/fullcalendar@3.9.0/dist/fullcalendar.min.css" rel="stylesheet">
 <link rel="stylesheet" href="park.css">
+<link rel="stylesheet" href="css/responsive.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
