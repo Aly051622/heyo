640fb094ba4e6cc207be7a2345b2b016f3b87f81
reignya <rhyniaimpas7@gmail.com>
2024-11-24 16:55:11 +0800

    Update dashboard.php

--- users/dashboard.php
+++ users/dashboard.php
@@ -95,6 +95,11 @@ if (strlen($_SESSION['vpmsuid']==0)) {
             overflow-x: auto;
             font-family:Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
             z-index: -1;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            background: whitesmoke;
+            height: 100vh;
         }
         /* Carousel container and styling */
         .carousel-container {
