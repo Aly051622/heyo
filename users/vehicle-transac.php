a803ae6dbff6fadd6be778a94313de9117a6a3be
reignya <rhyniaimpas7@gmail.com>
2024-11-26 20:37:21 +0800

    Update vehicle-transac.php

--- users/vehicle-transac.php
+++ users/vehicle-transac.php
@@ -57,7 +57,8 @@ if (strlen($_SESSION['vpmsuid'] == 0)) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
-
+    <link rel="stylesheet" href="../admin/assets/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="../admin/assets/css/style.css">
     <style>
         #printbtn:hover,
         #viewbtn:hover {
