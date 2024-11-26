ce7ffddf99ef500c5213e4fda20101af0ecae615
reignya <rhyniaimpas7@gmail.com>
2024-11-26 22:28:36 +0800

    Update view-vehicle.php

--- users/view-vehicle.php
+++ users/view-vehicle.php
@@ -50,7 +50,7 @@ if (strlen($_SESSION['vpmsuid']==0)) {
           .breadcrumbs {
               width: 30%;
               background-color: #ffffff;
-              padding: 3px;
+              padding: 1px;
               border-radius: 5px;
               box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
               margin-bottom: 10px;
@@ -172,7 +172,7 @@ if (strlen($_SESSION['vpmsuid']==0)) {
 <div class="right-panel">
     <div class="breadcrumbs">
         <div class="breadcrumbs-inner">
-            <div class="row m-3">
+            <div class="row m-1">
                 <div class="col-sm-4">
                     <div class="page-header float-left">
                         <div class="page-title">
