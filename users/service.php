916ffe7c5d27c848498aa29bb69d1317044997c8
reignya <rhyniaimpas7@gmail.com>
2024-11-27 01:22:40 +0800

    Update service.php

--- users/service.php
+++ users/service.php
@@ -21,8 +21,8 @@ if (strlen($_SESSION['vpmsuid']==0)) {
 <link rel="stylesheet" href="../admin/assets/css/cs-skin-elastic.css">
 <link rel="stylesheet" href="../admin/assets/css/style.css">
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800" rel="stylesheet" type="text/css">
-<link rel="apple-touch-icon" href="ctul.png">
-<link rel="shortcut icon" href="ctul.png">
+<link rel="apple-touch-icon" href="images/ctu.png">
+<link rel="shortcut icon" href="images/ctu.png">
 <link href="https://cdn.jsdelivr.net/npm/chartist@0.11.0/dist/chartist.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/jqvmap@1.5.1/dist/jqvmap.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/weathericons@2.1.0/css/weather-icons.css" rel="stylesheet">
