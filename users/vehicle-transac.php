a7c3f6267b3c80a8bfdcea1cba877c422ab6baf7
reignya <rhyniaimpas7@gmail.com>
2024-11-26 19:41:51 +0800

    Update vehicle-transac.php

--- users/vehicle-transac.php
+++ users/vehicle-transac.php
@@ -321,7 +321,7 @@ WHERE tblqr_login.ContactNumber = '88888888888' ";
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
-<script src="../admin/assets/js/main.js"></script>
+
 
 
 </body>
