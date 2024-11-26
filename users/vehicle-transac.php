76fe4d684a33119ae85682acd6f91afc309be583
reignya <rhyniaimpas7@gmail.com>
2024-11-27 00:14:22 +0800

    Update vehicle-transac.php

--- users/vehicle-transac.php
+++ users/vehicle-transac.php
@@ -89,14 +89,14 @@ if (strlen($_SESSION['vpmsuid'] == 0)) {
 
         /* Breadcrumb styles */
         .breadcrumbs {
-            width: 90%;
+            width: 30%;
             background-color: #ffffff;
-            padding: 7px;
+            padding: 3px;
             border-radius: 5px;
             box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
             margin-bottom: 10px;
             margin-top: 10px;
-            margin-left: 5em;
+            margin-left: 1em;
         }
 
         .breadcrumbs .breadcrumb {
@@ -307,7 +307,7 @@ if (strlen($_SESSION['vpmsuid'] == 0)) {
     <div class="breadcrumbs">
         <div class="breadcrumbs-inner">
             <div class="row m-0">
-                <div class="col-sm-3">
+                <div class="col-sm-4">
                     <div class="page-header float-left">
                         <div class="page-title">
                             <h1>Vehicle Logs</h1>
