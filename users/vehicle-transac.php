92584abefb49f0bbcf5882c0af77581e2ce58d2c
reignya <rhyniaimpas7@gmail.com>
2024-11-26 20:25:06 +0800

    Update vehicle-transac.php

--- users/vehicle-transac.php
+++ users/vehicle-transac.php
@@ -59,7 +59,6 @@ if (strlen($_SESSION['vpmsuid'] == 0)) {
     <link rel="shortcut icon" href="https://upload.wikimedia.org/wikipedia/commons/9/9a/CTU_new_logo.png">
 
 
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
@@ -131,8 +130,6 @@ body{
             <div class="animated fadeIn">
                 <div class="row">
                    
-         
-
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-header">
@@ -158,7 +155,12 @@ body{
                                     }
 
                                     $cnt = 1;
-                                    while ($row = mysqli_fetch_array($result)) { ?>
+                                    while ($row = mysqli_fetch_array($result)) {
+                                        // Debugging: Print the row structure
+                                        echo "<pre>";
+                                        var_dump($row);
+                                        echo "</pre>";
+                                        ?>
                                         <tr>
                                             <td><?php echo $cnt; ?></td>
                                             <td><?php echo $row['ParkingSlot']; ?></td>
@@ -178,8 +180,6 @@ body{
                 </div>
             </div>
 
-   
-
         </div>
     </div><!-- .animated -->
 </div><!-- .content -->
@@ -196,7 +196,6 @@ body{
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
 <script src="../admin/assets/js/main.js"></script>
-
 
 </body>
 </html>
