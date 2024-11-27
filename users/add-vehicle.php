0598efa845c74c738eb1b4560b5dc6bcd3699b4e
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-27 15:39:21 +0800

    Update add-vehicle.php

--- users/add-vehicle.php
+++ users/add-vehicle.php
@@ -719,8 +719,7 @@ if (mysqli_query($con, $query)) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
-    <link rel="stylesheet" href="../admin/assets/css/cs-skin-elastic.css">
-    <link rel="stylesheet" href="../admin/assets/css/style.css">
+    <link rel="stylesheet" href="css/responsive.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
@@ -730,6 +729,7 @@ if (mysqli_query($con, $query)) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../admin/assets/css/cs-skin-elastic.css">
     <link rel="stylesheet" href="../admin/assets/css/style.css">
+
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
     <style>
@@ -935,13 +935,6 @@ function toggleOtherModelInput() {
 
 
         <div class="row">
-            <div class="col-lg-6">
-                <div class="card">
-                    
-                   
-                </div> <!-- .card -->
-
-            </div><!--/.col-->
 
 
             <!-- Plate Number Exists Modal -->
