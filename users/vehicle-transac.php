54350bbcadbc4f4eee446906143fe53882edc9f2
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-27 14:53:30 +0800

    Update vehicle-transac.php

--- users/vehicle-transac.php
+++ users/vehicle-transac.php
@@ -85,14 +85,13 @@ if (strlen($_SESSION['vpmsuid'] == 0)) {
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
     <style>
-        body {
+        html,body {
             font-family: 'Poppins', sans-serif;
             height: 100vh;
             margin: 0;
             padding: 0;
             overflow-x: auto;
             background: whitesmoke;
-            
             transition: all 0.3s ease;
         }
 
