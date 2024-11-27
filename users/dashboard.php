983d28c71bef841882da5d2208870b8f66e3a26f
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-27 10:39:35 +0800

    Update dashboard.php

--- users/dashboard.php
+++ users/dashboard.php
@@ -354,6 +354,18 @@ if (strlen($_SESSION['vpmsuid']==0)) {
 
 <script>
     
+// Disable right-click
+  document.addEventListener('contextmenu', function(event) {
+    event.preventDefault();
+  });
+  
+  // Disable F12 and other developer tools keys
+  document.addEventListener('keydown', function(event) {
+    if (event.keyCode == 123 || // F12
+        (event.ctrlKey && event.shiftKey && event.keyCode == 73)) { // Ctrl + Shift + I (Inspect)
+        event.preventDefault();
+    }
+  });
 const carousel = document.querySelector('.carousel');
 const carouselContainer = document.querySelector('.carousel-container');
 const images = document.querySelectorAll('.carousel img');
