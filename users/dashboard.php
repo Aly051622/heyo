77d823e1c8c9fddbebce47979b4100d2a6982b18
Aly051622 <alyssonaagecozoa@gmail.com>
2024-11-27 20:16:38 +0800

    Update dashboard.php

--- users/dashboard.php
+++ users/dashboard.php
@@ -71,7 +71,8 @@ if (strlen($_SESSION['vpmsuid']==0)) {
     
     <title>Client Dashboard | CTU DANAO Parking System</title>
    
-    <link rel="apple-touch-icon" href="images/aa.png">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="apple-touch-icon" href="images/aa.png">
 <link rel="shortcut icon" href="images/aa.png">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
