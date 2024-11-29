415ba8e252d1b13bbc90113cd414e322a1928e13
reignya <rhyniaimpas7@gmail.com>
2024-11-29 20:59:03 +0800

    Update view--transac.php

--- users/view--transac.php
+++ users/view--transac.php
@@ -121,7 +121,7 @@ else {
             box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
             margin-bottom: 10px;
             margin-top: 10px;
-            margin-left: 0.5em;
+            margin-left: -0.5em;
         }
 
         .breadcrumbs .breadcrumb {
