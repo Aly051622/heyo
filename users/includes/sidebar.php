3c1c4fab834437d0c2b1789db17aa8cbc056a531
reignya <rhyniaimpas7@gmail.com>
2024-11-29 21:30:18 +0800

    Update sidebar.php

--- users/includes/sidebar.php
+++ users/includes/sidebar.php
@@ -1,144 +1,39 @@
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 <head>
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QFW9zO27sR9L5N9l9X3C9yDhhFLDf8OiN+RFpF2mVXIOcn4Thhm/6z9y2mbsVsZ1" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HoAq9iHknG3+MBHpNPIc4ni+lK69CBddA/PJyt7jyvTFDhoJfWl7r29wh+I1kFTV" crossorigin="anonymous"></script>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72gFWwHE1tvtfQN2fvHldDpWl9QUZT3G33asP1l61v0Ya34xUkTljAlbiY1" crossorigin="anonymous">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.2/js/bootstrap.bundle.min.js" integrity="sha384-GWqAb6aDZ5DJVPbqwnBp4ycg4CJyZ3sEkkquLDVfuFxIt6sXJ3dKEyk5L1hZ4Epl" crossorigin="anonymous"></script>
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/your-kit-code.js" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
-<script src="https://kit.fontawesome.com/your-kit-code.js" crossorigin="anonymous"></script>
-</head> 
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap');
-body, * {
-    font-family: 'Open Sans', sans-serif !important; /* Ensure Open Sans is prioritized */
-    margin: 0; /* Reset margin for consistency */
-    padding: 0; /* Reset padding for consistency */
-    box-sizing: border-box; /* Avoid layout issues */
-}
-    .left-panel{
-        margin-top: 12px;
-        border-top: groove 2px;
-    }
-    #sidebar {
-    width: 200px;
-    position: fixed;
-    left: 0;
-    height: 100bh;
-    overflow: hidden;
-    transition: width 0.3s ease;
-
-    z-index: -1;
-}
-
-#sidebar.collapsed {
-    width: 70px;
-}
-
-#toggleSidebar {
-    color: white;
-    border: none;
-    left: 10px;
-}
-
-
-/*sidebarrrrr */
-
-@media (max-width: 768px) {
-    #sidebar {
-        left: -250px;
-    }
-
-    #sidebar.collapsed {
-        left: 0;
-    }
-
-    #toggleSidebar {
-        display: block;
-    }
-
-    #right-panel {
-        margin-left: 0;
-    }
-}
-
-@media (max-width: 576px) {
-    #sidebar {
-        width: 200px;
-        margin-top: -15px;
-    }
-
-    #sidebar.collapsed {
-        width: 50px;
-    }
-
-    #toggleSidebar {
-        left: 5px;
-        top: 5px;
-    }
-}
-
-@media (max-width: 480px) {
-    #sidebar {
-        width: 200px;
-        margin-top: -15px;
-    }
-
-    #sidebar.collapsed {
-        width: 50px;
-    }
-
-    #toggleSidebar {
-        left: 5px;
-        top: 5px;
-    }
-}
-</style> 
+</head>
 <aside id="left-panel" class="left-panel">
     <nav class="navbar navbar-expand-sm navbar-default">
         <div id="main-menu" class="main-menu collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="active">
-                    <a href="dashboard.php" style="text-decoration: none;">
+                    <a href="dashboard.php">
                         <i class="menu-icon fa fa-laptop"></i>Dashboard
                     </a>
                 </li>
                 <li>
-                    <a href="view-vehicle.php"  style="text-decoration: none;">
-                    <i class="menu-icon fa fa-car-home"></i> Owned Vehicle/s
+                    <a href="view-vehicle.php">
+                    <i class="menu-icon fa fa-car-side"></i> Owned Vehicle/s
                     </a>
                 </li>
                 <li>
-                    <a href="vehicle-transac.php"  style="text-decoration: none;">
-                        <i class="menu-icon fa fa-house"></i>Vehicle Logs
+                    <a href="vehicle-transac.php">
+                        <i class="menu-icon fa fa-address-book"></i>Vehicle Logs
                     </a>
                 </li>
                 <li>
-                    <a href="add-vehicle.php"  style="text-decoration: none;">
+                    <a href="add-vehicle.php">
                         <i class="menu-icon fa fa-address-book"></i>Register Vehicle
                     </a>
                 </li>
                 <li>
-                    <a href="service.php"  style="text-decoration: none;">
+                    <a href="maps.php">
+                        <i class="menu-icon fa fa-map-location-dot"></i>Maps
+                    </a>
+                </li>
+                <li>
+                    <a href="service.php">
                         <i class="menu-icon fa fa-headset"></i>Chat Concern
                     </a>
                 </li>
