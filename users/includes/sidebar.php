e33fbe9491bc1406b9920f444c9caaf86b9499c5
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-01 11:33:03 +0800

    Update sidebar.php

--- users/includes/sidebar.php
+++ users/includes/sidebar.php
@@ -1,10 +1,8 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 <head>
-
+<link rel="stylesheet" href="css/responsive.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 <script src="https://kit.fontawesome.com/your-kit-code.js" crossorigin="anonymous"></script>
 </head> 
 <style>
@@ -92,38 +90,37 @@ body, * {
         top: 5px;
     }
 }
+
 </style> 
 <aside id="left-panel" class="left-panel">
     <nav class="navbar navbar-expand-sm navbar-default">
         <div id="main-menu" class="main-menu collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li class="active">
-                    <a href="dashboard.php" style="text-decoration: none;">
-                        <i class="menu-icon fa fa-laptop"></i>Dashboard
+                <li class="active" >
+                    <a href="dashboard.php">
+                        <i class="menu-icon fa fa-laptop" style="  text-decoration: none;"></i>Dashboard
                     </a>
                 </li>
                 <li>
-                    <a href="view-vehicle.php"  style="text-decoration: none;">
+                    <a href="view-vehicle.php">
+                        <i class="menu-icon fa fa-truck"></i>View Vehicle
+                    </a>
+                </li>
+                <li>
+                    <a href="view-vehicless.php">
                     <i class="menu-icon fa fa-car-side"></i> Owned Vehicle/s
                     </a>
                 </li>
                 <li>
-                    <a href="vehicle-transac.php"  style="text-decoration: none;">
-                        <i class="menu-icon fa fa-truck"></i>Vehicle Logs
+                    <a href="vehicle-transac.php">
+                        <i class="menu-icon fa fa-address-book"></i>Vehicle Logs
                     </a>
                 </li>
                 <li>
-                    <a href="add-vehicle.php"  style="text-decoration: none;">
-                        <i class="menu-icon fa fa-address-book"></i>Register Vehicle
-                    </a>
-                </li>
-                <!--
-                <li>
-                    <a href="service.php"  style="text-decoration: none;">
+                    <a href="service.php">
                         <i class="menu-icon fa fa-headset"></i>Chat Concern
                     </a>
                 </li>
--->
             </ul>
         </div>
     </nav>
