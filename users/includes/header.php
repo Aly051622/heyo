fbbd1fd88567de4deb88f08c6deb61e00a5bd530
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-02 16:55:18 +0800

    Update header.php

--- users/includes/header.php
+++ users/includes/header.php
@@ -344,9 +344,9 @@ body, * {
                                     <div class="form-inline"></div>
                                 </div>
 
-                            <div class="user-area dropdown float-right">
+                            <div class="user-area dropdown float-right" style="margin-top: -100px;">
                                 <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <img class="user-avatar rounded-circle" src="../admin/images/images.png" alt="User Avatar" style="margin-top: -100px;">
+                                    <img class="user-avatar rounded-circle" src="../admin/images/images.png" alt="User Avatar" >
                                 </a>
 
                                 <div class="user-menu dropdown-menu" id="hh">
