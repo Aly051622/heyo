da8faf43ca9742a8dcf5b7a067c87b8e4c5ce419
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-04 04:33:57 +0800

    Update profile.php

--- users/profile.php
+++ users/profile.php
@@ -403,7 +403,7 @@ html,body{
                                     <div class="imgp"><!-- START: File Uploads -->
                                         <div class="row form-group" id="for-registration-files" style="display: none;">
                                             <div class="col-md-3">
-                                                <label for="nv-file" class="form-control-label">Upload MV File</label>
+                                                <label for="nv-file" class="form-control">Upload MV File</label>
                                             </div>
                                             <div class="col-md-9">
                                                 <input type="file" id="nv-file" name="NV_image" accept=".jpeg, .jpg" class="form-control fil">
@@ -412,13 +412,13 @@ html,body{
 
                                         <div class="row form-group" id="registered-files" style="display: none;">
                                             <div class="col-md-3">
-                                                <label for="or-file" class="form-control-label">Upload OR File</label>
+                                                <label for="or-file" class="form-control">Upload OR File</label>
                                             </div>
                                             <div class="col-md-9">
                                                 <input type="file" id="or-file" name="OR_image" accept=".jpeg, .jpg" class="form-control fil">
                                             </div>
                                             <div class="col-md-3">
-                                                <label for="cr-file" class="form-control-label">Upload CR File</label>
+                                                <label for="cr-file" class="form-control">Upload CR File</label>
                                             </div>
                                             <div class="col-md-9">
                                                 <input type="file" id="cr-file" name="CR_image" accept=".jpeg, .jpg" class="form-control fil">
