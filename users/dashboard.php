e5960f72b9d7af1c5efbb771ae61ce4ebcf68f5c
Aly051622 <alyssonaagecozoa@gmail.com>
2024-12-04 09:09:39 +0800

    Update dashboard.php

--- users/dashboard.php
+++ users/dashboard.php
@@ -61,6 +61,7 @@ if (strlen($_SESSION['vpmsuid'] == 0)) {
       <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
 
 
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/normalize.css@8.0.0/normalize.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
